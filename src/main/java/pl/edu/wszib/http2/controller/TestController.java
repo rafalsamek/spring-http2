430c4d7f7d10138133ee5c4016afb6f458df4287
rsamek <rafal.samek@gmail.com>
2024-04-13 10:37:58 +0200

    TestController

---
+++ src/main/java/pl/edu/wszib/http2/controller/TestController.java
@@ -0,0 +1,17 @@
+package pl.edu.wszib.http2.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping
+    public String test(Model model) {
+        return "test";
+    }
+}
