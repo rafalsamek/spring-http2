59a7b5040f1e25bb508d8d07bf1aee49fe7317a8
rsamek <rafal.samek@gmail.com>
2024-04-13 15:03:26 +0200

    ToDoController

--- src/main/java/pl/edu/wszib/http2/controller/ToDoController.java
+++ src/main/java/pl/edu/wszib/http2/controller/ToDoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.edu.wszib.http2.service.ToDoService;
 import pl.edu.wszib.http2.service.model.ToDo;
@@ -21,17 +22,20 @@ public class ToDoController {
 
     @GetMapping
     public String list(Model model) {
-        List<ToDo> toDoList = new ArrayList<>();
-        for (ToDoStatus status : ToDoStatus.values()) {
-            ToDo toDo = new ToDo();
-            toDo.setId(status.ordinal());
-            toDo.setTermin("2024-05-30");
-            toDo.setStatus(status);
-            toDo.setZadanie(String.format("Test Todo for status : %s", status));
-            toDoList.add(toDo);
-        }
-
-        model.addAttribute("todos", toDoList);
+        model.addAttribute("todos", toDoService.list());
         return "todo/list";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("newTodo", new ToDo());
+        return "todo/create";
+    }
+
+    @PostMapping("/create")
+    public String createAction(ToDo newTodo, Model model) {
+        newTodo.setStatus(ToDoStatus.NEW);
+        toDoService.create(newTodo);
+        return "redirect:/todos";
     }
 }
