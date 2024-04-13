156e3ae1d84b7b2a800296c8defbc09af895538d
rsamek <rafal.samek@gmail.com>
2024-04-13 10:48:08 +0200

    TestController

--- src/main/java/pl/edu/wszib/http2/controller/TestController.java
+++ src/main/java/pl/edu/wszib/http2/controller/TestController.java
@@ -5,14 +5,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/test")
 public class TestController {
 
     @GetMapping
-    public String test(Model model) {
-        model.addAttribute("name", "Rafał");
+    public String test(Model model, @RequestParam String name) {
+        model.addAttribute("name", name);
         return "test";
     }
 }
